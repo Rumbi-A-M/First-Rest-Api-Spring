c2a1456dfc35caf19b100ccefc77510bd9ed57eb
Audrey.Dev <rumbidzaiamateke@gmail.com>
2026-04-23 23:29:21 +0200

    added exception handling

--- src/main/java/pl/edu/vistula/firstrestapispring/product/repository/ProductRepository.java
+++ src/main/java/pl/edu/vistula/firstrestapispring/product/repository/ProductRepository.java
@@ -1,19 +1,9 @@
 package pl.edu.vistula.firstrestapispring.product.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.edu.vistula.firstrestapispring.product.domain.Product;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class ProductRepository {
-
-    private final Map<Long, Product> database = new HashMap<>();
-    private Long index = 1L;
-
-    public Product save(Product product) {
-        product.setId(index);
-        database.put(index, product);
-        index++;
-        return product;
-    }
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
